21f0f21851d79d2a09b7719069a69d91ef69ff0d
jdklatskin <45906408+jdklatskin@users.noreply.github.com>
2022-12-08 13:25:39 -0500

    Update streamlit_app.py
    Added a box to search for a fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,12 @@ streamlit.dataframe(fruits_to_show)
 # Fruityvice API section
 streamlit.header("Fruityvice Fruit Advice!")
 
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# Add a fruit choice text box
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 # Variable cleans up the json
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
