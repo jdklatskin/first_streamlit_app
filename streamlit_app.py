8a69566adcf3abfabd35dfbf30585be8eb5bdacb
jdklatskin <45906408+jdklatskin@users.noreply.github.com>
2022-12-08 13:26:56 -0500

    Update streamlit_app.py
    Fixed bug in fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,11 +26,12 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # Add a fruit choice text box
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # Variable cleans up the json
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Put the cleaned up response into a pandas dataframe and displays it as a table
