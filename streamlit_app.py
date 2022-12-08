5f9e5694f9a37fc9b2a81cf0f30c8bb96ca2f62c
jdklatskin <45906408+jdklatskin@users.noreply.github.com>
2022-12-08 14:47:55 -0500

    Update streamlit_app.py
    removed extra line in snowflake section

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,9 +61,6 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
-
-streamlit.dataframe(my_data_rows)
-
 #second fruit input box
 add_my_fruit = streamlit.text_input('What fruit would you like to add??','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
